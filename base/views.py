46b310c6a8e094b006b3e04aade1f32538a5c9db
Krapp Ramiro <krappramiro@disroot.org>
2022-11-23 10:17:41 -0300

    added Message model

--- base/views.py
+++ base/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers lounge'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend developers lounge'},
+# ]
 
 
 def home(request):
-    return render(request, 'base/home.html', {'rooms':rooms})
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request, pk): #pk comes from urls.py
-    room = None
-    for r in rooms:
-        if r['id'] == int(pk):
-            room = r
+def room(request, pk):  # pk comes from urls.py
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
